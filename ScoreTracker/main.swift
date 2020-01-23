3c0ded6c6899cb218faf2c6f6d8ac6b9bc0cf19e
Szabolcs Toth <tsz@purzelbaum.hu>
2020-01-23 21:15:01 +0100

    Initial Commit

---
+++ ScoreTracker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScoreTracker
+//
+//  Created by Szabolcs Toth on 23/01/2020.
+//  Copyright © 2020 purzelbaum.hu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
